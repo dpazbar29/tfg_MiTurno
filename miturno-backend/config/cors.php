0c5dd7519bb8a76e960a63bcab949f30500bb2b7
dpazbar29 <145806563+dpazbar29@users.noreply.github.com>
2026-05-05 10:32:58 +0200

    Cambio cors.php

--- miturno-backend/config/cors.php
+++ miturno-backend/config/cors.php
@@ -7,6 +7,8 @@ return [
     'allowed_methods' => ['*'],
     
     'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
         'https://miturno-frontend-production.up.railway.app',
         'https://miturno-frontend-production.railway.app'
     ],
